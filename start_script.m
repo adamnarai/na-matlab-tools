5fc689f2f42a3ce3fe4426f23574005241bfc1c7
Narai Adam <narai.adam@gmail.com>
2018-06-28 14:55:34 +0200

    start_script.m 'path' option added

--- start_script.m
+++ start_script.m
@@ -1,16 +1,22 @@
-function p = start_script()
-clc
-tic
+function p = start_script(mode)
+if nargin < 1
+    mode = '';
+end
 
 % Add na-matlab-toolbox subfolders
 addpath(genpath(fileparts(mfilename('fullpath'))));
+add_subpath('bin');
 
-% Get caller
-st = dbstack;
-try
-    scriptName = st(2).name;
-catch
-    scriptName = 'Script';
+if isempty(mode)
+    clc
+    tic
+    % Get caller
+    st = dbstack;
+    try
+        scriptName = st(2).name;
+    catch
+        scriptName = 'Script';
+    end
+    
+    p = start_log(scriptName);
 end
-
-p = start_log(scriptName);
