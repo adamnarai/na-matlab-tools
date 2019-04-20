2b0f70495b560e1424e72d7a357e59adb0b00ea0
Ádám Nárai <narai.adam@gmail.com>
2019-04-20 19:38:15 +0200

    checking path validity in get_work_path.m

--- get_work_path.m
+++ get_work_path.m
@@ -1,19 +1,32 @@
-function WorkPath = get_work_path(levels)
-% Get the project path based on this functions location
+function work_path = get_work_path(levels)
+% GET_WORK_PATH  Get the project folder.
+% Relies on the relative location of this script.
 %
 % INPUT:
 %           levels = indicates how deep is this function in the directory
-%                    structure (0 means the WorkPath)
+%                    structure (0 means the work_path)
 %
 % Adam Narai, RCNS HAS, 2018
+%
 
-% Default, assuming WorkPath/bin/external/na-matlab-tools structure
+% Default, assuming work_path/bin/external/na-matlab-tools structure
 if nargin < 1
     levels = 3;
 end
 
-WorkPath = fileparts(mfilename('fullpath'));
+% Get path
+work_path = fileparts(mfilename('fullpath'));
 for i = 1:levels
-    WorkPath = fileparts(WorkPath);
+    work_path = fileparts(work_path);
+end
+
+% Check if the original caller is from this working dir
+st = dbstack('-completenames');
+caller_path = st(end).file;
+if ~all(ismember(work_path, caller_path))
+    error(['The found work path (', work_path, ') is invalid.', char(10),...
+        'get_work_path() was found in ', mfilename('fullpath'), char(10),...
+        'however the caller is from ', caller_path,...
+        'You can resolve this error by adding only the valid get_work_path() to Matlab path.']);
 end
 
