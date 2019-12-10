fb0c8c2b06a5e367f615545e7c762c89af90cf80
Ádám Nárai <narai.adam@gmail.com>
2019-12-10 15:08:08 +0100

    cleaning run_limo_1st_level.m

--- eeg/glm/run_limo_1st_level.m
+++ eeg/glm/run_limo_1st_level.m
@@ -1,44 +1,18 @@
-function run_limo_1st_level(LIMO, Y, contr)
-if nargin < 3
-    contr = [];
-end
-
+function run_limo_1st_level(LIMO, Y)
 create_dir(LIMO.dir);
 cd(LIMO.dir);
 
 % Create design matrix
 [LIMO.design.X, LIMO.design.nb_conditions, LIMO.design.nb_interactions,...
-    LIMO.design.nb_continuous] = limo_design_matrix(Y, LIMO,0);
+    LIMO.design.nb_continuous] = limo_design_matrix(Y, LIMO, 0);
 
+% Set the LIMO struct
 LIMO.design.status = 'to do';
 save LIMO LIMO; clear Y
 
 % Run GLM estimation
 load LIMO LIMO
-limo_eeg_nofigure(4);
-
-% Compute contrasts
-if ~isempty(contr)
-    disp('Computing contrasts');
-    load LIMO
-    load Yr
-    load Betas
-    
-    if isfield(LIMO, 'contrast')
-        LIMO = rmfield(LIMO, 'contrast');
-    end
-    
-    % Create contrast
-    if iscell(contr)
-        for contrIdx = idx
-            currContr = contr{contrIdx};
-            LIMO = run_contrast(LIMO, Yr, Betas, currContr, contrIdx);
-        end
-    else
-        LIMO = run_contrast(LIMO, Yr, Betas, contr);
-    end
-end
-save LIMO LIMO
+limo_eeg(4);
 
 % Delete Yr, Yhat and Res files
 if exist('Yr.mat', 'file')
@@ -50,14 +24,4 @@ end
 if exist('Res.mat', 'file')
     delete('Res.mat');
 end
-
-function LIMO = run_contrast(LIMO, Yr, Betas, contr, idx)
-if nargin < 5
-    idx = 1;
-end
-C = limo_contrast_checking(LIMO.dir, LIMO.design.X, contr);
-LIMO.contrast{idx}.C = C;
-LIMO.contrast{idx}.V = 'T';
-% T test, results saved as con_x.mat
-limo_contrast(Yr, Betas, LIMO, 0, 1);
 
